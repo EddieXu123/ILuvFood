8aaa03addc9bc5c4e44e3b8735eab069b7e9ef10
Alex Liu <liualexander19@gmail.com>
2021-03-13 18:39:55 -0500

    cleanup

--- lib/screens/home/customer/customer_home_controller.dart
+++ lib/screens/home/customer/customer_home_controller.dart
@@ -23,15 +23,6 @@ class CustomerHomeController extends StatefulWidget {
 /// This is the private State class that goes with CustomerHomeControllert.
 class _CustomerHomeControllerState extends State<CustomerHomeController> {
   int _currentIndex = 0;
-  // static const TextStyle optionStyle =
-  //     TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-  // static
-
-  // void _onItemTapped(int index) {
-  //   setState(() {
-  //     _currentIndex = index;
-  //   });
-  // }
 
   @override
   Widget build(BuildContext context) {
@@ -51,31 +42,6 @@ class _CustomerHomeControllerState extends State<CustomerHomeController> {
                 icon: new Icon(Icons.account_circle), label: "Account")
           ]),
     );
-    // return Scaffold(
-    //   body: Center(
-    //     child: _widgetOptions.elementAt(_currentIndex),
-    //   ),
-    //   bottomNavigationBar: BottomNavigationBar(
-    //     backgroundColor: MyColors.myPurple,
-    //     items: const <BottomNavigationBarItem>[
-    //       BottomNavigationBarItem(
-    //         icon: Icon(Icons.home),
-    //         label: 'Home',
-    //       ),
-    //       BottomNavigationBarItem(
-    //         icon: Icon(Icons.account_circle),
-    //         label: 'Account',
-    //       ),
-    //       BottomNavigationBarItem(
-    //         icon: Icon(Icons.favorite),
-    //         label: 'Favorites',
-    //       ),
-    //     ],
-    //     currentIndex: _currentIndex,
-    //     selectedItemColor: MyColors.myPink,
-    //     onTap: _onItemTapped,
-    //   ),
-    // );
   }
 
   void onTabTapped(int index) {
@@ -84,67 +50,3 @@ class _CustomerHomeControllerState extends State<CustomerHomeController> {
     });
   }
 }
-
-// import 'package:flutter/material.dart';
-// import 'package:iluvfood/screens/home/customer/customer_home.dart';
-// import 'package:iluvfood/screens/home/customer/customer_profile.dart';
-// import 'package:iluvfood/shared/constants.dart';
-// import 'package:iluvfood/shared/errorPage.dart';
-
-// /// This is the stateful widget that the main application instantiates.
-// class CustomerHomeController extends StatefulWidget {
-//   @override
-//   _CustomerHomeControllerState createState() => _CustomerHomeControllerState();
-// }
-
-// /// This is the private State class that goes with CustomerHomeControllert.
-// class _CustomerHomeControllerState extends State<CustomerHomeController> {
-//   int _selectedIndex = 0;
-//   static const TextStyle optionStyle =
-//       TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-//   static List<Widget> _widgetOptions = <Widget>[
-//     // maps page
-//     CustomerHome(),
-
-//     // account page
-//     CustomerProfile(),
-
-//     // favorites page
-//     ErrorPage(),
-//   ];
-
-//   void _onItemTapped(int index) {
-//     setState(() {
-//       _selectedIndex = index;
-//     });
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: Center(
-//         child: _widgetOptions.elementAt(_selectedIndex),
-//       ),
-//       bottomNavigationBar: BottomNavigationBar(
-//         backgroundColor: MyColors.myPurple,
-//         items: const <BottomNavigationBarItem>[
-//           BottomNavigationBarItem(
-//             icon: Icon(Icons.home),
-//             label: 'Home',
-//           ),
-//           BottomNavigationBarItem(
-//             icon: Icon(Icons.account_circle),
-//             label: 'Account',
-//           ),
-//           BottomNavigationBarItem(
-//             icon: Icon(Icons.favorite),
-//             label: 'Favorites',
-//           ),
-//         ],
-//         currentIndex: _selectedIndex,
-//         selectedItemColor: MyColors.myPink,
-//         onTap: _onItemTapped,
-//       ),
-//     );
-//   }
-// }
