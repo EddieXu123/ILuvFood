b16c95fd71d66d6551e9079b2e71c75700c62a2c
awashburn <ajw228@case.edu>
2021-03-25 15:40:22 -0400

    added special icons for status of order

--- lib/screens/home/customer/order_history.dart
+++ lib/screens/home/customer/order_history.dart
@@ -7,6 +7,7 @@ import 'package:iluvfood/services/auth.dart';
 import 'package:iluvfood/services/database.dart';
 import 'package:iluvfood/shared/loading.dart';
 import 'package:provider/provider.dart';
+import 'package:iluvfood/shared/constants.dart';
 
 class OrderHistory extends StatefulWidget {
   final Customer customer;
@@ -20,6 +21,14 @@ class OrderHistory extends StatefulWidget {
 class _OrderHistoryState extends State<OrderHistory> {
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
   final _formKey = GlobalKey<FormState>();
+
+  Icon getStatus(Order order) {
+    // TODO - change this to if the status is complete
+    if (order.orderId == "orderId") {
+      return Icon(Icons.check, color: Colors.purple[300], size: 30.0);
+    }
+    return Icon(Icons.sync, color: Colors.purple[300], size: 30.0);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -43,22 +52,57 @@ class _OrderHistoryState extends State<OrderHistory> {
                       delegate: SliverChildListDelegate([
                         for (var i = customerOrders.length - 1; i >= 0; i--)
                           StreamBuilder<Order>(
-                            stream:
-                                DatabaseService(uid: customerOrders[i]).customerOrder,
+                            stream: DatabaseService(uid: customerOrders[i])
+                                .customerOrder,
                             builder: (context, snapshot) {
                               if (snapshot.hasData) {
                                 Order order = snapshot.data;
                                 DateTime dateTime = order.dateTime;
                                 return Card(
-                                  child: ListTile(
-                                      title: Text(order.businessName +
-                                          " : " +
-                                          dateTime.month.toString() +
-                                          "-" +
-                                          dateTime.day.toString() +
-                                          "-" +
-                                          dateTime.year.toString()),
-                                      subtitle: Text(order.orderId),
+                                  elevation: 8.0,
+                                  margin: new EdgeInsets.symmetric(
+                                      horizontal: 10.0, vertical: 6.0),
+                                  child: Container(
+                                    decoration:
+                                        BoxDecoration(color: Colors.white24),
+                                    child: ListTile(
+                                      contentPadding: EdgeInsets.symmetric(
+                                          horizontal: 20.0, vertical: 10.0),
+                                      leading: Container(
+                                        padding: EdgeInsets.only(right: 12.0),
+                                        decoration: new BoxDecoration(
+                                          border: new Border(
+                                            right: new BorderSide(
+                                                width: 1.0,
+                                                color: Colors.purple[300]),
+                                          ),
+                                        ),
+                                        child: getStatus(order),
+                                      ),
+                                      title: Text(
+                                        order.businessName +
+                                            " : " +
+                                            dateTime.month.toString() +
+                                            "-" +
+                                            dateTime.day.toString() +
+                                            "-" +
+                                            dateTime.year.toString(),
+                                        style: TextStyle(
+                                            color: Colors.purple[300],
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 17.0),
+                                      ),
+                                      subtitle: Row(
+                                        children: <Widget>[
+                                          Text("Order Id: " + order.orderId,
+                                              style: TextStyle(
+                                                  color: Colors.purple[300],
+                                                  fontSize: 17.0))
+                                        ],
+                                      ),
+                                      trailing: Icon(Icons.keyboard_arrow_right,
+                                          color: Colors.purple[300],
+                                          size: 30.0),
                                       onTap: () {
                                         Navigator.push(
                                             context,
@@ -66,7 +110,9 @@ class _OrderHistoryState extends State<OrderHistory> {
                                                 builder: (context) =>
                                                     OrderDetails(
                                                         order: order)));
-                                      }),
+                                      },
+                                    ),
+                                  ),
                                 );
                               } else {
                                 return Loading();
