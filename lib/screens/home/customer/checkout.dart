22f5bea298a54e99755fafa36cf6a811fb7f563c
Eddie Xu <50087436+EddieXu123@users.noreply.github.com>
2021-03-27 11:02:35 -0400

    Checkout, Pickup date+time, Order confirmation/summary, Tracking (#24)
    * Added UI for checkout, confirmation, tracking
    
    * Added tracking page
    
    * Finished with reorganizing tracking
    
    * Finished tracking
    
    * Added empty-cart-prevention, (-) quantity (+) in checkout, and home button on order_summary
    
    * reduce height for overflow issue
    
    Co-authored-by: Alex Liu <liualexander19@gmail.com>

--- lib/screens/home/customer/checkout.dart
+++ lib/screens/home/customer/checkout.dart
@@ -6,6 +6,9 @@ import 'package:iluvfood/screens/home/customer/order_summary.dart';
 import 'package:iluvfood/services/database.dart';
 import 'package:provider/provider.dart';
 import 'package:iluvfood/models/business.dart';
+import 'pickup.dart';
+import 'customer_page_style.dart';
+import 'package:toast/toast.dart';
 import 'package:toast/toast.dart';
 import 'package:mailer/mailer.dart';
 import 'package:mailer/smtp_server.dart';
@@ -27,22 +30,32 @@ showAlertDialog(BuildContext context) {
     onPressed: () async {
       print("processing order!");
       try {
-        await DatabaseService().initializePastOrder(Order(
-            orderId: "orderId", // TODO - determine how to generate an orderId
-            dateTime: DateTime.now(),
-            businessUid: cart.businessUid,
-            customerUid: user.uid,
-            businessName: cart.businessName,
-            customerName: "customerName",
-            items: cart.cartItems));
-        // on add, take them to a summary page
-        Navigator.of(context).popUntil((route) => route.isFirst);
+        if (cart.priceInCart == 0) {
+          Toast.show("Your Cart is Empty!", context,
+              duration: 2, gravity: Toast.CENTER);
+        } else {
+          await DatabaseService().initializePastOrder(Order(
+              orderId: "orderId", // TODO - determine how to generate an orderId
+              dateTime: DateTime.now(),
+              businessUid: cart.businessUid,
+              customerUid: user.uid,
+              businessName: cart.businessName,
+              customerName: "customerName",
+              items: cart.cartItems));
+          // on add, take them to a summary page
+          // Navigator.pop(context);
+          Navigator.of(context).popUntil((route) => route.isFirst);
 
-        Navigator.push(
-            context, MaterialPageRoute(builder: (context) => OrderSummary()));
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => OrderSummary()));
+          print("Resetting After Purchase");
+          cart.reset();
+        }
+
         await sendMail();
 
         // Resetting cart at the VERY END
+        // Unless we want to do this in order_summary.dart, which we probs do
         cart.reset();
       } catch (e) {
         print("Error adding to order history: $e");
@@ -80,7 +93,7 @@ sendMail() async {
   // Create our message.
   final message = Message()
     ..from = Address(username, 'ILuvFood')
-    ..recipients.add('erx@case.edu') // Customer email here
+    ..recipients.add('iluvfood64@case.edu') // Customer email here
     // ..ccRecipients.addAll(['destCc1@example.com', 'destCc2@example.com'])
     // ..bccRecipients.add(Address('bccAddress@example.com'))
     ..subject =
@@ -130,44 +143,148 @@ class _CartList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var itemNameStyle = Theme.of(context).textTheme.headline6;
+    bool selected_date = false;
+    bool selected_time = false;
     // This gets the current state of CartModel and also tells Flutter
     // to rebuild this widget when CartModel notifies listeners (in other words,
     // when it changes).
     // var cart = context.watch<CartModel>();
     CartModel cart = context.watch<CartModel>();
+    // var itemPrice = "-1";
     return new Scaffold(
-      body: ListView.builder(
-        itemCount: cart.cartItems.length,
-        itemBuilder: (context, index) => ListTile(
-          leading: Column(mainAxisSize: MainAxisSize.min, children: <Widget>[
-            IconButton(
-              icon: Icon(Icons.delete),
-              onPressed: () {
-                cart.delete(cart.cartItems[index].uid);
-              },
+        body: Column(children: <Widget>[
+      Container(
+        height: 225,
+        child: Container(
+          height: 250,
+          child: ListView.builder(
+            itemCount: cart.cartItems.length,
+            itemBuilder: (context, index) => ListTile(
+              leading:
+                  Column(mainAxisSize: MainAxisSize.min, children: <Widget>[
+                Container(
+                  width: 15,
+                  child: IconButton(
+                    icon: Icon(Icons.delete),
+                    onPressed: () {
+                      cart.delete(cart.cartItems[index].uid);
+                    },
+                  ),
+                ),
+              ]),
+              trailing: Row(mainAxisSize: MainAxisSize.min, children: <Widget>[
+                IconButton(
+                  icon: Icon(Icons.remove_circle_outline),
+                  onPressed: () {
+                    // itemPrice = "${cart.cartItems[index].price}";
+                    cart.remove(cart.cartItems[index].uid);
+                    // print(itemPrice);
+                  },
+                ),
+                Container(
+                  width: 30.0,
+                  child: Text(
+                    "(${cart.cartItems[index].quantity})",
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                Container(
+                  width: 20,
+                  child: IconButton(
+                    icon: Icon(Icons.add_circle_outline),
+                    onPressed: () {
+                      cart.add(cart.cartItems[index].uid);
+                    },
+                  ),
+                ),
+              ]),
+              title: Text(
+                // itemPrice == "-1"
+                "${cart.cartItems[index].item}", // LATER: (\$${cart.cartItems[index].price})",
+                // : "${cart.cartItems[index].item} ($itemPrice)", //(${cart.cartItems[index].quantity})",
+                style: itemNameStyle,
+              ),
             ),
-          ]),
-          trailing: Row(mainAxisSize: MainAxisSize.min, children: <Widget>[
-            IconButton(
-              icon: Icon(Icons.remove_circle_outline),
-              onPressed: () {
-                cart.remove(cart.cartItems[index].uid);
-              },
-            ),
-            IconButton(
-              icon: Icon(Icons.add_circle_outline),
-              onPressed: () {
-                cart.add(cart.cartItems[index].uid);
-              },
-            ),
-          ]),
-          title: Text(
-            "${cart.cartItems[index].item} (${cart.cartItems[index].quantity})",
-            style: itemNameStyle,
           ),
         ),
       ),
-    );
+      Container(
+        height: 1,
+        color: Colors.grey,
+      ),
+      Container(
+        child: Container(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Pick up Date",
+                style: headingStyle,
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    // TODO: Make them into buttons and use This.Date + 3 days
+                    // Need to talk with team to decide how long actually
+                    dateWidget("Wed", "24 March", true), // ThisDate + 3
+                    dateWidget("Thu", "25 March", false), // ThisDate + 4
+                    dateWidget("Fri", "26 March", false), // etc
+                    dateWidget("Sat", "27 March", false),
+                    dateWidget("Mon", "28 March", false),
+                    dateWidget("Tue", "29 March", false)
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                height: 1,
+                color: Colors.grey,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                "Pick up Time",
+                style: headingStyle,
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    // DITTO THE ABOVE
+                    TextButton(
+                      onPressed: () {
+                        selected_time = true;
+                      },
+                      child: timeWidget("10:00 AM to 12:00 PM", selected_time),
+                    ),
+                    InkWell(
+                      onTap: () {},
+                      child: timeWidget("12:00 PM to 02:00 PM", true),
+                    ),
+                    timeWidget("02:00 PM to 04:00 PM", false),
+                    timeWidget("04:00 PM to 06:00 PM", false),
+                  ],
+                ),
+              ),
+              // SizedBox(
+              //   height: 20,
+              // ),
+            ],
+          ),
+        ),
+      ),
+    ]));
   }
 }
 
@@ -209,8 +326,13 @@ class _PurchaseNow extends StatelessWidget {
           children: [
             TextButton(
               onPressed: () {
-                print("processing order!");
-                showAlertDialog(context);
+                if (cart.priceInCart == 0) {
+                  Toast.show("Your Cart is Empty!", context,
+                      duration: 2, gravity: Toast.CENTER);
+                } else {
+                  print("processing order!");
+                  showAlertDialog(context);
+                }
               },
               child: Text(
                 'Place Order',
@@ -222,4 +344,49 @@ class _PurchaseNow extends StatelessWidget {
       ),
     );
   }
+}
+
+Container dateWidget(String day, String date, bool isActive) {
+  return Container(
+    margin: EdgeInsets.only(right: 10),
+    padding: EdgeInsets.all(10),
+    decoration: BoxDecoration(
+        color: (isActive) ? Colors.orange : Colors.grey.withOpacity(0.3),
+        borderRadius: BorderRadius.all(Radius.circular(20))),
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          day,
+          style: contentStyle.copyWith(
+              color: (isActive) ? Colors.white : Colors.black, fontSize: 20),
+        ),
+        Text(
+          date,
+          style: contentStyle.copyWith(
+              color: (isActive) ? Colors.white : Colors.black, fontSize: 13),
+        )
+      ],
+    ),
+  );
+}
+
+Container timeWidget(String time, bool isActive) {
+  return Container(
+    margin: EdgeInsets.only(right: 10),
+    padding: EdgeInsets.all(12),
+    decoration: BoxDecoration(
+        color: (isActive) ? Colors.orange : Colors.grey.withOpacity(0.3),
+        borderRadius: BorderRadius.all(Radius.circular(20))),
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          time,
+          style: contentStyle.copyWith(
+              color: (isActive) ? Colors.white : Colors.black, fontSize: 15),
+        ),
+      ],
+    ),
+  );
 }
